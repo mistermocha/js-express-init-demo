6d93cf8c7fb5218978a5d15fcb81f42544b16a60
Daniel Lounsbury <danielwlouns@gmail.com>
2023-05-26 10:05:21 -0700

    A full commitment's what I'm thinking of You wouldn't get this from any other guy

---
+++ server_3.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const port = 3000 
+
+app.get("/public/fruit", function (req,res)
+fruits = {
+apple: {border: "red", bg: ""}
+
+
+
+}
+
+
+
+)
+
+
+
+
+
+app.listen(port, function(){
+console.log('Example app listening on port ${port}!')
+
+})
